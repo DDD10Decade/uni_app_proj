efddd6d07f2dd3e6b64865a8aeb3d1e6778dbb06
DDD10Decade <102458086+DDD10Decade@users.noreply.github.com>
2023-09-25 22:56:11 +0800

    轮播图

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,16 @@
 <template>
 	<view>
-		
+		<!-- 轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<!-- 循环渲染轮播图的 item 项 -->
+			<swiper-item v-for="(item, i) in swiperList" :key="i">
+				       <view class="swiper-item">
+					         <!-- 动态绑定图片的 src 属性 -->
+					         <image :src="item.image_src"></image>
+					       </view>
+				     </swiper-item>
+			   
+		</swiper>
 	</view>
 </template>
 
@@ -8,12 +18,45 @@
 	export default {
 		data() {
 			return {
-				
+				//轮播图数据列表
+				swiperList: [],
 			};
-		}
+		},
+		onLoad() {
+			// 在小程序页面刚加载的时候，调用获取轮播图数据的方法
+			this.getSwiperList()
+		},
+		methods: {
+			// 获取轮播图数据的方法
+			async getSwiperList() {
+				//  发起请求
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				// 请求失败
+				if (res.meta.status !== 200) {
+					return uni.showToast({
+						title: '数据请求失败！',
+						duration: 1500,
+						icon: 'none',
+					})
+				}
+				// 请求成功，为 data 中的数据赋值
+				this.swiperList = res.message
+			},
+		},
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
 </style>
